d00b2b0f634826efbabb546593fa4a59b9c3474f
mfnalex <1122571+mfnalex@users.noreply.github.com>
2021-12-22 04:15:58 +0100

    Trying to get it running on Java 8

--- src/main/java/de/jeff_media/customblocks/implentation/OraxenBlock.java
+++ src/main/java/de/jeff_media/customblocks/implentation/OraxenBlock.java
@@ -22,3 +22,42 @@ public class OraxenBlock extends CustomBlock {
         return null;
     }
 }
+
+/*
+package de.jeff_media.customblocks.implentation;
+
+import de.jeff_media.customblocks.CustomBlock;
+import de.jeff_media.jefflib.exceptions.InvalidBlockDataException;
+import io.th0rgal.oraxen.mechanics.provided.gameplay.noteblock.NoteBlockMechanicFactory;
+import org.bukkit.block.Block;
+
+import java.lang.reflect.Method;
+
+public class OraxenBlock extends CustomBlock {
+
+    private static boolean disabled = false;
+    private static final Class<?> noteBlockMechanicFactoryClass;
+    private static final Object noteBlockMechanicFactoryInstance;
+    private static final Method setBlockModelMethod;
+
+    static {
+
+    }
+
+    public OraxenBlock(String id) throws InvalidBlockDataException {
+        super(id);
+        if(NoteBlockMechanicFactory.getInstance().getMechanic(id) == null) throw new InvalidBlockDataException("Could not find Oraxen block: " + id);
+    }
+
+    @Override
+    public void place(Block block) {
+        NoteBlockMechanicFactory.setBlockModel(block, getId());
+    }
+
+    @Override
+    public String getNamespace() {
+        return null;
+    }
+}
+
+ */
